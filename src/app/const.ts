35be17820aca991dacc1d499864bcaa99f4112f8
arturvoloshyn <arturvoloshyn@gmail.com>
2024-03-01 19:30:27 +0200

    feat(app): add consts

---
+++ src/app/const.ts
@@ -0,0 +1,18 @@
+export const ROUTES = {
+  DASHBOARD: {
+    path: '/',
+    title: 'Dashboard',
+  },
+  POST_CREATE: {
+    path: '/post/add',
+    title: 'Post Create',
+  },
+  POST_UPDATE: {
+    path: '/post/update',
+    title: 'Post Update',
+  },
+  POSTS: {
+    path: '/posts',
+    title: 'Posts',
+  },
+};
